efbf97bdd84001afc13a3a5250286e7267d87677
anangbima <50908265+anangbima@users.noreply.github.com>
2024-10-13 18:38:23 +0700

    today

---
+++ resources/js/Components/Input.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+        Name
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane"/>
+      <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+    </div>
+  )
+}
+
+export default Input
--- resources/js/Pages/Admin/Project.jsx
+++ resources/js/Pages/Admin/Project.jsx
@@ -3,17 +3,35 @@ import AdminLayout from '../../Layouts/AdminLayout'
 import { Head } from '@inertiajs/react'
 
 const Project = ({project}) => {
+
+
   return (
     <AdminLayout>
       <Head title='Project' />
 
       <div className='project'>
-        <div>
-          
+        <div className='mb-3'>
+          <a href="#" className=''>Create New</a>
         </div>
 
-        <div className='card'>
-          Hai
+        <form className=''>
+          <input 
+            type="text" 
+            name='name' 
+            id='name' 
+            placeholder='name' 
+          />
+
+          <textarea 
+            name="description" 
+            id="description"
+          ></textarea>
+
+          <input type="submit" value='Submit' />
+        </form>
+
+        <div className='bg-white p-5 rounded-xl shadow mt-3'>
+          Project 1
         </div>
       </div>
     </AdminLayout>
